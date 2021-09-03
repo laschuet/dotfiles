ee798f9e60d6b686084c41663f8ec0815b7fd8d5
laschuet <laschuet@users.noreply.github.com>
2021-09-03 12:55:47 +0200

    Change monospace font

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface document-font-name 'DejaVu Sans 11'
 gsettings set org.gnome.desktop.interface font-name 'DejaVu Sans 11'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Hack 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Iosevka Fixed Extended 11'
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
